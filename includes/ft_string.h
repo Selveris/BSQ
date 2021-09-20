0057b6015a27575fda961fe4cd160ca834fbd632
Bruno Goncalves de oliveira <bgoncalv@c1r12s2.42lausanne.ch>
2021-09-20 17:26:35 +0200

    first commit

---
+++ includes/ft_string.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_string.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgoncalv <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/20 17:19:23 by bgoncalv          #+#    #+#             */
+/*   Updated: 2021/09/20 17:19:26 by bgoncalv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_STRING_H
+# define FT_STRING_H
+
+void	ft_putchar(char c);
+void	ft_putstring(char *str);
+void	ft_atoi(char *str);
+int		ft_is_space(char c);
+int		is_charset(char *charset);
+
+#endif
